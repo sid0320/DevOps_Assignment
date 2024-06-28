6515a1a659c7e02aecbac22fe302a8f19dc46a2e
sid0320 <siddharthd77@gmail.com>
2024-06-28 19:15:19 +0530

    Implemented new feature: added new API endpoint

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -16,3 +16,15 @@ def about():
 @app.get("Contact-Us")
 def contact_us():
     return {"Email":"abc@gmail.com","Mob_no.":"1234567890"}
+
+@app.get("/login")
+def login(username : str,password : str):
+    if username == "mkg" and password == "abc12345":
+        return{"Data":"Login This is a login page"}
+    else: 
+        return{"Data":"Login failed incorrect username or password incorrect"}
+    
+@app.post("/login")
+def login_post(request:Request):
+    print(request.headers)
+    print(request.body)
